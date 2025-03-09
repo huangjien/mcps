8d6e6745b4f3df4263667adbabfb6405930ea585
huangjien <huangjien@gmail.com>
2025-03-09 12:05:19 +0000

    init

---
+++ src/main/java/com/huangjien/ai/mcps/McpsApplication.java
@@ -0,0 +1,13 @@
+package com.huangjien.ai.mcps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/huangjien/ai/mcps/McpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangjien.ai.mcps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
